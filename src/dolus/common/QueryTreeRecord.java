d1c0fa70d2ac0d11c96a5f106653b46051802034
m.amin <m.amin.rayej@gmail.com>
2019-01-02 22:41:14 +0330

    Implement constructors of QueryTreeRecord.

--- src/dolus/common/QueryTreeRecord.java
+++ src/dolus/common/QueryTreeRecord.java
@@ -1,4 +1,49 @@
 package dolus.common;
 
-public class QueryTreeRecord extends QueryRecord {
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * QueryTreeRecord nodes act as a tree node.
+ * Each node has access to its parent and children.
+ * Children are indexed from left to right starting with zero.
+ * Children of a QueryTreeNode can be accessed with their index.
+ *
+ * @author m.amin rayej
+ * @version 1.0
+ * @since 1.0
+ */
+public class QueryTreeRecord<K,V> extends QueryRecord<K,V> {
+
+    /**
+     * Contains the record of the queries nested in the current query
+     */
+    private List<QueryTreeRecord> children;
+
+    /**
+     * Default constructor of the QueryTreeRecord.
+     * It sets the parent of the current record to null and initializes the symbol table and children list.
+     *
+     * @since 1.0
+     */
+    public QueryTreeRecord(){
+
+        this(null);
+    }
+
+    /**
+     * It sets the parent of the current record and initializes the symbol table and children list
+     * @param parent previous active query.
+     *
+     * @since 1.0
+     */
+    public QueryTreeRecord(QueryTreeRecord<K,V> parent){
+        super(parent, new HashMap<>());
+
+        this.children = new ArrayList<>();
+    }
+
+
 }
