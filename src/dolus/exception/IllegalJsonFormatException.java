febdf524b23f963c0e8d6fd12e6ba2a53da4c838
m.amin <m.amin.rayej@gmail.com>
2019-01-05 21:51:28 +0330

    Adding exception package.

---
+++ src/dolus/exception/IllegalJsonFormatException.java
@@ -0,0 +1,14 @@
+package dolus.exception;
+
+/**
+ * Exception when JSON input syntax is not legal
+ *
+ * @author m.amin rayej
+ * @since 1.0
+ */
+public class IllegalJsonFormatException extends Exception{
+
+    public IllegalJsonFormatException(String msg){
+        super(msg);
+    }
+}
