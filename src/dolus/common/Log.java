d639440272496372c04a06bdd161fd5dc7cf2f77
m.amin <m.amin.rayej@gmail.com>
2019-01-06 15:57:58 +0330

    Add logging package

---
+++ src/dolus/common/Log.java
@@ -0,0 +1,11 @@
+package dolus.common;
+
+/**
+ * This class has static methods to work with the log file of Dolus
+ *
+ * @author m.amin rayej
+ * @version 1.0
+ * @since 1.0
+ */
+public class Log {
+}
