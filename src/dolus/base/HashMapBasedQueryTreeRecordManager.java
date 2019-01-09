52d2b606f433520c098528e5f9cc3908d0c0f5ca
m.amin <m.amin.rayej@gmail.com>
2019-01-10 00:12:03 +0330

    Add HashMapBasedQueryTreeRecordManager

---
+++ src/dolus/base/HashMapBasedQueryTreeRecordManager.java
@@ -0,0 +1,12 @@
+package dolus.base;
+
+/**
+ * This class manages HashMapBasedQueryRecords
+ *
+ * @param <K>
+ * @param <V>
+ */
+public class HashMapBasedQueryTreeRecordManager<K,V> extends QueryTreeRecordManager<K,V> {
+
+
+}
