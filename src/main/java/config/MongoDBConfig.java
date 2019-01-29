f03f9bf552b47aa2022f7056ab6b47e010864a4c
M.Amin Rayej <m.amin.rayej@gmail.com>
2019-01-29 23:50:03 +0330

    Modify MongoDBConfig.

--- src/main/java/config/MongoDBConfig.java
+++ src/main/java/config/MongoDBConfig.java
@@ -6,16 +6,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * Contains information about MongoDB database
+ * Contains information about MongoDB storage
  *
  * @author m.amin rayej
  * @version 1.0
  * @since 1.0
  */
-public class MongoDBConfig extends StorageConfig {
+public class MongoDBConfig<T extends StorageConfig> extends StorageConfig<T> {
 
     /**
-     * Component name of the logging system
+     * Component name to use in the logging system
      */
     private static final String componentName = "MongoDBConfig";
 
@@ -25,15 +25,18 @@ public class MongoDBConfig extends StorageConfig {
     private HashSet<String> collections;
 
     /**
-     * Contains mapping between collection name and its primary key
+     * Contains mapping between collection names and their primary keys
      */
     private HashMap<String, String> primaryKeys;
 
     /**
-     * Wraps a MongoDBConfig object around the configs of a MongoDB database
+     * Wraps a MongoDBConfig object around the configs of a MongoDB storage
      *
      * @param collections set of collection names
-     * @param primaryKeys mapping between collection name and its primary key
+     * @param primaryKeys mapping between collection names and their primary keys
+     * @param id          unique identifier of this storage
+     * @param engine      engine to use when querying data of this storage
+     * @param parent      parent of this storage in storage graph
      * @param host        host address
      * @param port        port number
      * @param database    database name
@@ -41,11 +44,39 @@ public class MongoDBConfig extends StorageConfig {
      * @param password    password credentials
      * @since 1.0
      */
-    public MongoDBConfig(HashSet<String> collections, HashMap<String,String> primaryKeys, String host, String port, String database, String username, String password) {
+    public MongoDBConfig(HashSet<String> collections, HashMap<String,String> primaryKeys,
+                         String id, String engine, T parent, String host, String port, String database, String username, String password) {
 
-        super(host, port, database, username, password);
+        super(id, engine, parent, host, port, database, username, password);
         this.collections = collections;
         this.primaryKeys = primaryKeys;
+
+        Log.log(String.format("MongoDB Storage: %s is registered with parent: %s", id, parent.getId()), componentName, Log.INFORMATION);
+
+    }
+
+    /**
+     * Wraps a MongoDBConfig object around the configs of a MongoDB storage
+     *
+     * @param collections set of collection names
+     * @param primaryKeys mapping between collection names and their primary keys
+     * @param id          unique identifier of this storage
+     * @param engine      engine to use when querying data of this storage
+     * @param host        host address
+     * @param port        port number
+     * @param database    database name
+     * @param username    username credentials
+     * @param password    password credentials
+     * @since 1.0
+     */
+    public MongoDBConfig(HashSet<String> collections, HashMap<String,String> primaryKeys,
+                         String id, String engine, String host, String port, String database, String username, String password) {
+
+        super(id, engine, host, port, database, username, password);
+        this.collections = collections;
+        this.primaryKeys = primaryKeys;
+
+        Log.log(String.format("MongoDB Storage: %s is registered", id), componentName, Log.INFORMATION);
 
     }
 
@@ -65,7 +96,7 @@ public class MongoDBConfig extends StorageConfig {
      * Return primary key of the specified collection
      *
      * @param collectionName name of the collection
-     * @return PK of the table
+     * @return PK of the table , null if collection does not exist
      * @since 1.0
      */
     public String getPrimaryKey(String collectionName) {
