dbfba6913afec11ec38e19d5797ce321d415ce17
M.Amin Rayej <m.amin.rayej@gmail.com>
2019-01-29 23:44:11 +0330

    Modify MySqlConfig.

--- src/main/java/config/MySqlConfig.java
+++ src/main/java/config/MySqlConfig.java
@@ -6,48 +6,80 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * Contains information about MySQL database
+ * Contains information about MySQL storage
  *
  * @author m.amin rayej
  * @version 1.0
  * @since 1.0
  */
-public class MySqlConfig extends StorageConfig {
+public class MySqlConfig<T extends StorageConfig> extends StorageConfig<T> {
 
     /**
-     * Component name of the logging system
+     * Component name to use in the logging system
      */
     private static final String componentName = "MySqlConfig";
 
     /**
      * Contains information about each table
+     * it is a map between table name and its columns : table name -> [column1, column2 ,...]
      */
     private HashMap<String, HashSet<String>> tablesInfo;
 
     /**
-     * Contains mapping between table name and its primary key
+     * Contains mapping between table names and their primary keys
      */
     private HashMap<String, String> primaryKeys;
 
     /**
-     * Wraps a MySqlConfig object around the configs of a MySQL database
+     * Wraps a MySqlConfig object around configs of a MySQL storage
      *
-     * @param tablesInfo  information about tables of the database
-     * @param primaryKeys mapping between table name and its primary key
-     * @param host        host address of database
-     * @param port        port number of database
+     * @param tablesInfo  information about tables of the storage
+     * @param primaryKeys mapping between table names and their primary keys
+     * @param id          unique identifier of this storage
+     * @param engine      engine to use when querying data of this storage
+     * @param parent      parent of this storage in storage graph
+     * @param host        host address of storage
+     * @param port        port number of storage
      * @param database    database name
      * @param username    username credential
      * @param password    password credential
      * @since 1.0
      */
     public MySqlConfig(HashMap<String, HashSet<String>> tablesInfo, HashMap<String, String> primaryKeys,
-                       String host, String port, String database, String username, String password) {
+                       String id, String engine, T parent, String host, String port, String database, String username, String password) {
 
-        super(host, port, database, username, password);
+        super(id, engine, parent, host, port, database, username, password);
 
         this.tablesInfo = tablesInfo;
         this.primaryKeys = primaryKeys;
+
+        Log.log(String.format("MySQL Storage: %s is registered with parent: %s", id, parent.getId()), componentName, Log.INFORMATION);
+
+    }
+
+    /**
+     * Wraps a MySqlConfig object around configs of a MySQL storage
+     *
+     * @param tablesInfo  information about tables of the storage
+     * @param primaryKeys mapping between table names and their primary keys
+     * @param id          unique identifier of this storage
+     * @param engine      engine to use when querying data of this storage
+     * @param host        host address of storage
+     * @param port        port number of storage
+     * @param database    database name
+     * @param username    username credential
+     * @param password    password credential
+     * @since 1.0
+     */
+    public MySqlConfig(HashMap<String, HashSet<String>> tablesInfo, HashMap<String, String> primaryKeys,
+                       String id, String engine, String host, String port, String database, String username, String password) {
+
+        super(id, engine, host, port, database, username, password);
+
+        this.tablesInfo = tablesInfo;
+        this.primaryKeys = primaryKeys;
+
+        Log.log(String.format("MySQL Storage: %s is registered", id), componentName, Log.INFORMATION);
 
     }
 
@@ -62,7 +94,7 @@ public class MySqlConfig extends StorageConfig {
     public boolean containsColumn(String tableName, String columnName) {
 
         if (!tablesInfo.containsKey(tableName)) {
-            Log.log(String.format("Table %s is not defined in MySQL database", tableName), componentName, Log.ERROR);
+            Log.log(String.format("Table %s is not defined in MySQL storage", tableName), componentName, Log.ERROR);
             return false;
         }
 
@@ -73,13 +105,13 @@ public class MySqlConfig extends StorageConfig {
      * Return primary key of the specified table
      *
      * @param tableName name of the table
-     * @return PK of the table
+     * @return PK of the table or null if table does not exist
      * @since 1.0
      */
     public String getPrimaryKey(String tableName) {
 
         if (!primaryKeys.containsKey(tableName)) {
-            Log.log(String.format("Table %s is not defined in MySQL database", tableName), componentName, Log.ERROR);
+            Log.log(String.format("Table %s is not defined in MySQL storage", tableName), componentName, Log.ERROR);
             return null;
         }
 
@@ -87,7 +119,7 @@ public class MySqlConfig extends StorageConfig {
     }
 
     /**
-     * Check whether MySQL database contains the table or not
+     * Check whether MySQL storage contains the table or not
      *
      * @param tableName name of the table
      * @return true if MySQL contains the table, false otherwise
