3608e05541ca39702715f327dbde768f4e991b96
m.amin rayej <m.amin.rayej@gmail.com>
2019-02-06 23:35:26 +0330

    Code explanation.

--- src/main/java/config/ConfigUtilities.java
+++ src/main/java/config/ConfigUtilities.java
@@ -3,7 +3,7 @@ package config;
 import common.Log;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +11,12 @@ import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 
 /**
- * This class is responsible for reading and parsing the config files of Dolus
+ * This class is responsible for loading config files of Dolus
+ *
+ * There are two config files. main config file and storage config file.
+ * main config file contains global configurations and tweaks about dolus.
+ * it determines where necessary files are, how dolus should behave and etc.
+ * storage config file contains information about what storage systems are present and how they are arranged.
  *
  * @author m.amin rayej
  * @version 1.0
@@ -30,19 +35,27 @@ public class ConfigUtilities {
     private static String storageConfigDir;
 
     /**
-     * Available storage types
+     * Valid storage types
      */
-    private static String[] storageTypes = {"mysql", "mongodb"};
+    private static String[] validStorageTypes = {"mysql", "mongodb"};
 
     /**
-     * Available query engines
+     * Valid query engines
      */
-    private static String[] engines = {"apache_drill"};
+    private static String[] validEngines = {"apache_drill"};
 
     /**
      * List of all storage systems present in storage config
      * this list actually contains just top level storage systems
      * child storage systems can be access through their parents.
+     * this data structure represent storage graph.
+     * example:
+     *    [ mysql1  ----> mongo1 ----> mysql2 ]
+     *     /     \                       |
+     *  mongo2  mongo3                 mongo4
+     *
+     *  this data structure only contains mysql1, mongo1, mysql2
+     *  other storage systems can be accessed by these three.
      */
     private static List<StorageConfig> storageConfigList;
 
@@ -59,41 +72,61 @@ public class ConfigUtilities {
     /**
      * Loads the main config file
      *
-     * @param configDir path to main config file
+     * @param mainConfigDir path to main config file
      * @return true if loading configuration was successful, false otherwise
      * @since 1.0
      */
-    public static boolean loadMainConfig(String configDir) {
+    public static boolean loadMainConfig(String mainConfigDir) {
 
-        File configFile = new File(configDir);
+        /*
+         * this method has three phase:
+         * 1- Checking phase: checks whether main config file exists and is readable
+         * 2- Reading phase : reads the content of the file
+         * 3- Parsing phase : parses contents of the file
+         * and if all three phases were successful, it configures the system based on extracted configurations.
+         */
 
-        //check whether config file exists and dolus has read permission
-        if (!configFile.exists()) {
-            Log.log("Main config file can not be located: " + configDir, componentName, Log.ERROR);
+        ////////////////////// Checking phase //////////////////////////
+        File mainConfigFile = new File(mainConfigDir);
+
+        //check whether main config file exists and dolus has permission to read
+        if (!mainConfigFile.exists()) {
+            Log.log("Main config file can not be located here: " + mainConfigDir, componentName, Log.ERROR);
             return false;
-        } else if (!configFile.canRead()) {
+        } else if (!mainConfigFile.canRead()) {
             Log.log("Dolus does not have permission to read the main config file", componentName, Log.ERROR);
             return false;
         }
 
+        ////////////////////// Reading phase //////////////////////////
         //read contents of the config file
-        String configContent = readConfigFileContents(configFile);
-        if (configContent == null) {
+        String mainConfigContent = readConfigFileContents(mainConfigFile);
+        if (mainConfigContent == null) {
             Log.log("Something went wrong during reading contents of the main configuration file", componentName, Log.ERROR);
             return false;
         }
 
-        Log.log("Contents of the main config file read successfully", componentName, Log.INFORMATION);
+        Log.log("Contents of the main configuration file read successfully", componentName, Log.INFORMATION);
 
-        //parse config content and extract configurations
+        ////////////////////// Parsing phase //////////////////////////
+        /*
+         * data structure to contain main configuration contents.
+         * main config file contains some key value pairs for example: "log_dir" : "/home/user/...".
+         * this object is used to store these key value pairs.
+         */
         HashMap<String, String> configuration = new HashMap<>();
-        mainConfigLoaded = ConfigParser.parseMainConfigFileContents(configContent, configuration);
 
+        //parse mainConfigContent and store its key value pair configurations in configurations object
+        mainConfigLoaded = ConfigParser.parseMainConfigFileContents(mainConfigContent, configuration);
+
+        //if main config contents parsed successfully
         if (mainConfigLoaded) {
             Log.log("Contents of the main config file parsed and stored successfully", componentName, Log.INFORMATION);
 
-            //configure storage config directory and log system based on extracted data
+            //configure storage directory
             storageConfigDir = configuration.get("storage_config_dir");
+
+            //configure logging system directory
             Log.setLogDir(configuration.get("log_dir"));
         } else
             Log.log("Storing main configuration failed", componentName, Log.ERROR);
@@ -109,25 +142,36 @@ public class ConfigUtilities {
      */
     public static boolean loadStorageConfig() {
 
+        /*
+         * this method has four phase:
+         * 1- Checking phase  : checks that storage config file exists, is readable and main config file was loaded successfully before
+         * 2- Reading phase   : reads the content of the file
+         * 3- Parsing phase   : parses contents of the file
+         * 4- Construct phase : constructs the storage graph based on storage systems specified in storage config file
+         */
+
+        ////////////////////// Checking phase //////////////////////////
+        //make sure main config file was loaded successfully
         if (!mainConfigLoaded) {
             Log.log("Main config file is not loaded. can not load storage config file", componentName, Log.ERROR);
             return false;
         }
 
-        File configFile = new File(storageConfigDir);
+        File storageConfigFile = new File(storageConfigDir);
 
-        //check whether config file exists and dolus has read permission
-        if (!configFile.exists()) {
-            Log.log("Storage config file can not be located: " + storageConfigDir, componentName, Log.ERROR);
+        //check whether config file exists and dolus has permission to read
+        if (!storageConfigFile.exists()) {
+            Log.log("Storage config file can not be located here: " + storageConfigDir, componentName, Log.ERROR);
             return false;
-        } else if (!configFile.canRead()) {
+        } else if (!storageConfigFile.canRead()) {
             Log.log("Dolus does not have permission to read the storage config file", componentName, Log.ERROR);
             return false;
         }
 
+        ////////////////////// Reading phase //////////////////////////
         //read contents of the config file
-        String configContent = readConfigFileContents(configFile);
-        if (configContent == null) {
+        String storageConfigContent = readConfigFileContents(storageConfigFile);
+        if (storageConfigContent == null) {
 
             Log.log("Something went wrong during reading contents of the storage configuration file", componentName, Log.ERROR);
             return false;
@@ -135,12 +179,13 @@ public class ConfigUtilities {
 
         Log.log("Contents of the storage config file read successfully", componentName, Log.INFORMATION);
 
+        ////////////////////// Parsing phase //////////////////////////
         //initialize storage config list
         storageConfigList = new ArrayList<>();
 
         /*
          * once scanning the storage config file is not enough to link parent and child storage systems
-         * because storage systems can appear in arbitrary orders in storage config file
+         * and construct the storage graph because storage systems can appear in arbitrary orders in storage config file
          * therefore parser store top level(parent) storage systems separately from child storage systems
          * then loops through the child storage systems and link each child to its parent
          * parents are stored in a hash map structure to be found fast
@@ -149,12 +194,13 @@ public class ConfigUtilities {
         //Hash map of top level storage systems: storage id -> storage object
         HashMap<String, StorageConfig> topLevelStorageSystems = new HashMap<>();
 
-        //List of storage systems having parents
+        //List of storage systems which have parents
         ArrayList<StorageConfig> children = new ArrayList<>();
 
-        //parse config content and extract configurations
-        storageConfigLoaded = ConfigParser.parseStorageConfigFileContents(configContent, storageConfigList, storageTypes, engines, topLevelStorageSystems, children);
+        //parse storage config content and extract configurations
+        storageConfigLoaded = ConfigParser.parseStorageConfigFileContents(storageConfigContent, storageConfigList, validStorageTypes, validEngines, topLevelStorageSystems, children);
 
+        ////////////////////// Construct phase //////////////////////////
         //create storage graph by connecting child and parent storage systems together
         for (StorageConfig child : children) {
 
@@ -184,7 +230,7 @@ public class ConfigUtilities {
     }
 
     /**
-     * Reads content of the config file
+     * Reads contents of the config file
      *
      * @param configFile configuration file
      * @return contents of the config file or null if an error occurs
@@ -202,14 +248,14 @@ public class ConfigUtilities {
                 content.append(scanner.nextLine());
 
             return content.toString();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             return null;
         }
 
     }
 
     /**
-     * Searches among registered storage systems to find one containing the named collection
+     * Searches among registered storage systems to find one which contains the named collection
      *
      * @param collectionName name of the collection
      * @return storage containing the named collection if found, null otherwise
@@ -217,31 +263,7 @@ public class ConfigUtilities {
      */
     public static StorageConfig findStorage(String collectionName) {
 
-        if (!storageConfigLoaded) {
-            Log.log("Storage config is not loaded. can not search for collections", componentName, Log.ERROR);
-            return null;
-        }
-
-        StorageConfig foundStorage = null;
-
-        //queue is used to walk through storage graph and visit top level storage systems first
-        ArrayBlockingQueue<StorageConfig> queue = new ArrayBlockingQueue<>(1);
-
-        queue.addAll(storageConfigList);
-
-        while (!queue.isEmpty()) {
-
-            StorageConfig current = queue.remove();
-
-            if (current.containsCollection(collectionName)) {
-                foundStorage = current;
-                break;
-            }
-
-            queue.addAll(current.getChildren());
-        }
-
-        return foundStorage;
+        return null;
     }
 
 }
