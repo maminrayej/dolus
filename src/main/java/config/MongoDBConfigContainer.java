d11ad7ba8c41dbf4a78c25dc41627b10cb322394
m.amin rayej <m.amin.rayej@gmail.com>
2019-02-07 01:37:43 +0330

    Code explanation.

--- src/main/java/config/MongoDBConfigContainer.java
+++ src/main/java/config/MongoDBConfigContainer.java
@@ -46,7 +46,7 @@ public class MongoDBConfigContainer extends StorageConfigContainer {
      * @param password    password credentials
      * @since 1.0
      */
-    public MongoDBConfigContainer(HashSet<String> collections, HashMap<String,String> primaryKeys,
+    public MongoDBConfigContainer(HashSet<String> collections, HashMap<String, String> primaryKeys,
                                   String id, String engine, String parentId, String host, String port, String database, String username, String password) {
 
         super(id, engine, parentId, host, port, database, username, password);
@@ -69,7 +69,7 @@ public class MongoDBConfigContainer extends StorageConfigContainer {
      * @param password    password credentials
      * @since 1.0
      */
-    public MongoDBConfigContainer(HashSet<String> collections, HashMap<String,String> primaryKeys,
+    public MongoDBConfigContainer(HashSet<String> collections, HashMap<String, String> primaryKeys,
                                   String id, String engine, String host, String port, String database, String username, String password) {
 
         super(id, engine, host, port, database, username, password);
@@ -78,11 +78,95 @@ public class MongoDBConfigContainer extends StorageConfigContainer {
 
     }
 
-    public MongoDBConfigContainer(){
+    /**
+     * Default constructor
+     *
+     * @since 1.0
+     */
+    public MongoDBConfigContainer() {
         super();
 
         this.collections = null;
         this.primaryKeys = null;
+    }
+
+    /**
+     * Parses MongoDB configurations
+     *
+     * @param mongoDBConfigContainer  container to store extracted configurations in
+     * @param mongoDBConfigJSONObject json object containing configurations
+     * @param validEngines            valid engines
+     * @param visitedIds              visited ids
+     * @return true if parsing and storing configurations was successful, false otherwise
+     */
+    public static boolean parseMongoDBConfig(MongoDBConfigContainer mongoDBConfigContainer, JSONObject mongoDBConfigJSONObject, String[] validEngines, HashSet<String> visitedIds) {
+
+        //first parse basic configurations
+        boolean successful = StorageConfigContainer.parseStorageConfig(mongoDBConfigContainer, mongoDBConfigJSONObject, validEngines, visitedIds);
+
+        //make sure parsing basic configurations was successful
+        if (!successful) {
+            Log.log("MySQL storage config parsing failed", componentName, Log.ERROR);
+            return false;
+        }
+
+        //get id of the current storage(is used for logging more helpful messages)
+        String id = mongoDBConfigContainer.getId();
+
+        //set of all collection names in mongodb database
+        HashSet<String> collections = new HashSet<>();
+
+        //map each collection name with its primary key
+        HashMap<String, String> primaryKeys = new HashMap<>();
+
+        //get array of collections present in the storage
+        JSONArray collectionArray = (JSONArray) mongoDBConfigJSONObject.get("collections");
+
+        if (collectionArray == null || collectionArray.size() == 0) {
+            Log.log("For MongoDB storage: " + id + ", collections attribute is not defined or it does not contain any collection", componentName, Log.ERROR);
+            return false;
+        }
+
+        //for each table, extract its meta data and store it
+        for (Object collectionObject : collectionArray) {
+
+            JSONObject collectionJsonObject = (JSONObject) collectionObject;
+
+            /////////////////// collection name //////////////////
+
+            //extract collection name
+            String collectionName = (String) collectionJsonObject.get("name");
+
+            //make sure collection name is specified
+            if (collectionName == null || collectionName.length() == 0) {
+                Log.log("For MongoDB storage: " + id + ", collection name attribute is not defined for one of its collections", componentName, Log.ERROR);
+                return false;
+            }
+
+            /////////////////// primary key //////////////////
+
+            //extract collection primary key
+            String collectionPrimaryKey = (String) collectionJsonObject.get("pk");
+
+            //if primary key is not defined or is empty -> set it to default mongoDB primary kry : _id
+            if (collectionPrimaryKey == null || collectionPrimaryKey.length() == 0)
+                collectionPrimaryKey = "_id";
+
+            //add extracted collection to set of collections
+            collections.add(collectionName);
+
+            //map current collection to its primary key
+            primaryKeys.put(collectionName, collectionPrimaryKey);
+        }
+
+        //store set of collections in container
+        mongoDBConfigContainer.setCollections(collections);
+
+        //store primary keys in container
+        mongoDBConfigContainer.setPrimaryKeys(primaryKeys);
+
+        return true;
+
     }
 
     public boolean containsCollection(String collectionName) {
@@ -100,69 +184,23 @@ public class MongoDBConfigContainer extends StorageConfigContainer {
         return primaryKeys.get(collectionName);
     }
 
+    /**
+     * Set collections set
+     *
+     * @param collections set of collections
+     * @since 1.0
+     */
     public void setCollections(HashSet<String> collections) {
         this.collections = collections;
     }
 
+    /**
+     * Set primary keys
+     *
+     * @param primaryKeys mapping between collection name and its primary key
+     * @since 1.0
+     */
     public void setPrimaryKeys(HashMap<String, String> primaryKeys) {
         this.primaryKeys = primaryKeys;
-    }
-
-    public static boolean parseMongoDBConfig(MongoDBConfigContainer mongoDBConfig, JSONObject mongoDBConfigJSONObject, String[] engines, HashSet<String> visitedIds) {
-
-        boolean successful = StorageConfigContainer.parseStorageConfig(mongoDBConfig, mongoDBConfigJSONObject, engines, visitedIds);
-
-        if (!successful){
-            Log.log("MySQL storage config parsing failed", componentName, Log.ERROR);
-            return false;
-        }
-
-        String id = mongoDBConfig.getId();
-
-        //storage meta data
-        HashSet<String> collections = new HashSet<>();//set of all collection names in mongodb database
-        HashMap<String, String> primaryKeys = new HashMap<>();//map each collection name with its primary key
-
-        //meta data extraction
-        JSONArray collectionArray = (JSONArray) mongoDBConfigJSONObject.get("collections");
-        if (collectionArray == null || collectionArray.size() == 0) {
-            Log.log("MongoDB collections attribute is not defined or it does not contain any collection for: " + id, componentName, Log.ERROR);
-            return false;
-        }
-
-        //meta data holders
-        String collectionName;
-        JSONObject collection;
-
-        //for each collection extract its name and primary key
-        for (Object collectionObject : collectionArray) {
-
-            collection = (JSONObject) collectionObject;
-
-            //extract collection name
-            collectionName = (String) collection.get("name");
-
-            if (collectionName == null || collectionName.length() == 0) {
-                Log.log("MongoDB collection name attribute is not defined for one of its collections for: " + id, componentName, Log.ERROR);
-                return false;
-            }
-
-            //extract collection primary key
-            String collectionPrimaryKey = (String) collection.get("pk");
-
-            //if primary key is not defined or is empty -> set it to default mongoDB primary kry : _id
-            if (collectionPrimaryKey == null || collectionPrimaryKey.length() == 0)
-                collectionPrimaryKey = "_id";
-
-            collections.add(collectionName);
-
-            primaryKeys.put(collectionName, collectionPrimaryKey);
-        }
-
-        mongoDBConfig.setCollections(collections);
-        mongoDBConfig.setPrimaryKeys(primaryKeys);
-
-        return true;
-
     }
 }
