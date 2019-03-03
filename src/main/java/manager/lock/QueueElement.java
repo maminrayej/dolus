e0579127c8efd13d19819205814f0aa4370189bf
maminrayej <m.amin.rayej@gmail.com>
2019-03-04 02:29:06 +0330

    Implemented QueueElement class as a public class.

---
+++ src/main/java/manager/lock/QueueElement.java
@@ -0,0 +1,91 @@
+package manager.lock;
+
+import manager.transaction.Transaction;
+
+/**
+ * This class is a container for transaction and its requested originalLock and
+ *
+ * @author m.amin.rayej
+ * @version 1.0
+ * @since 1.0
+ */
+public class QueueElement {
+
+    private Transaction transaction;
+    private Lock originalLock;
+    private Lock appliedLock;
+
+    /**
+     * Constructor
+     *
+     * @param transaction  transaction
+     * @param originalLock original requested lock by transaction
+     * @param appliedLock  lock this is applied to this element
+     * @since 1.0
+     */
+    public QueueElement(Transaction transaction, Lock originalLock, Lock appliedLock) {
+        this.transaction = transaction;
+        this.originalLock = originalLock;
+        this.appliedLock = appliedLock;
+    }
+
+    /**
+     * Get transaction
+     *
+     * @return transaction
+     * @since 1.0
+     */
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    /**
+     * Set transaction
+     *
+     * @param transaction transaction
+     * @since 1.0
+     */
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    /**
+     * Get originalLock
+     *
+     * @return originalLock
+     * @since 1.0
+     */
+    public Lock getOriginalLock() {
+        return originalLock;
+    }
+
+    /**
+     * Set originalLock
+     *
+     * @param originalLock originalLock
+     * @since 1.0
+     */
+    public void setOriginalLock(Lock originalLock) {
+        this.originalLock = originalLock;
+    }
+
+    /**
+     * Get applied lock
+     *
+     * @return applied lock
+     * @since 1.0
+     */
+    public Lock getAppliedLock() {
+        return appliedLock;
+    }
+
+    /**
+     * Set applied lock
+     *
+     * @param appliedLock lock to apply on this element
+     * @since 1.0
+     */
+    public void setAppliedLock(Lock appliedLock) {
+        this.appliedLock = appliedLock;
+    }
+}
