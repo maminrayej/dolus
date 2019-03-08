93a2f4c56135f6098cc44acf17bf7efa0c9de046
maminrayej <m.amin.rayej@gmail.com>
2019-03-08 21:54:45 +0330

    Implement Call back runnable class.

---
+++ src/main/java/manager/lock/CallBackRunnable.java
@@ -0,0 +1,82 @@
+package manager.lock;
+
+import manager.transaction.Transaction;
+
+import java.util.Queue;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * This class is a background thread to inform transactions of their new granted locks
+ *
+ * @author m.amin rayej
+ * @version 1.0
+ * @since 1.0
+ */
+public class CallBackRunnable implements Runnable {
+
+    private Queue<QueueElement> firstQueue;
+
+    private Queue<QueueElement> secondQueue;
+
+    private ReentrantLock firstQueueLock;
+
+    private ReentrantLock secondQueueLock;
+
+    public CallBackRunnable(Queue<QueueElement> firstQueue, Queue<QueueElement> secondQueue, ReentrantLock firstQueueLock, ReentrantLock secondQueueLock) {
+        this.firstQueue = firstQueue;
+        this.secondQueue = secondQueue;
+        this.firstQueueLock = firstQueueLock;
+        this.secondQueueLock = secondQueueLock;
+    }
+
+    @Override
+    public void run() {
+
+        while(true) {
+
+            //try to lock the first queue
+            if (firstQueueLock.tryLock()) {
+
+                //inform all transactions in the first queue of their granted locks
+                informTransaction(firstQueue);
+
+                //unlock the first queue
+                firstQueueLock.unlock();
+            }
+            else if (secondQueueLock.tryLock()) {
+
+                //inform all transactions in the first queue of their granted locks
+                informTransaction(secondQueue);
+
+                //unlock the second queue
+                secondQueueLock.unlock();
+            }
+        }
+
+    }
+
+    /**
+     * Loops through the given queue and inform each transaction of its granted lock
+     *
+     * @param queue queue of granted requests
+     * @since 1.0
+     */
+    private void informTransaction(Queue<QueueElement> queue) {
+
+        //loop through the queue and inform each transaction of its granted lock
+        for (int i = 0; i < queue.size(); i++) {
+
+            //get the head of the queue
+            QueueElement queueElement = queue.remove();
+
+            //get transaction
+            Transaction transaction = queueElement.getTransaction();
+
+            //get granted lock
+            Lock lock = queueElement.getAppliedLock();
+
+            //inform the transaction that requested lock is granted
+            transaction.lockIsGranted(lock);
+        }
+    }
+}
